de58a42cedfe0f64ee5969b62e9d4c2e680095d2
Giovanni Oggioni <gio14og@gmail.com>
2020-12-17 10:07:38 +0100

    start to build the navbar

---
+++ pages/Recipes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+
+export default function Recipes() {
+    return (
+        <div>
+            <Navbar />
+            <h1>QUI VANNO IL FILTER E LE RICETTE</h1>
+        </div>
+    )
+}
