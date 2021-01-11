e90135c214dc7b5d5c65b9b5b2985b2bf587cf97
Alberto Maria Carotenuto <carotenuto.a.m@gmail.com>
2021-01-11 16:19:26 +0100

    use effect fix

--- pages/Recipes.js
+++ pages/Recipes.js
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/client";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import React from "react";
 import TopNavbar from "./components/TopNavbar";
