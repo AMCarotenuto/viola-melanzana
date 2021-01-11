69b1c746608251781471890d6f2c1378c79f1f1b
Alberto Maria Carotenuto <carotenuto.a.m@gmail.com>
2021-01-11 18:08:07 +0100

    login check on homepage

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React from "react";
 import TopNavbar from "./components/TopNavbar";
 import InputIngredients from "./components/InputIngredients";
 import FilterRecipes from "./components/FilterRecipes";
 import Footer from "./components/Footer";
 
+function loginCheck() {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!(session || loading)) {
+      router.push("/");
+    }
+  }, [session, loading]);
+}
+
 export default function HomePage() {
-    return (
-        <div>
-            <TopNavbar />
-            <div className="main-homePage">
-            <br/>
-            <InputIngredients />
-            <h1>QUI VANNO LE CARDS, IL FILTER E LE RICETTE </h1>
-            <FilterRecipes />
-            <Footer />
-        </div>
-        </div>
-        
-    )
+  return (
+    <div>
+      <div>{loginCheck()}</div>
+      <TopNavbar />
+      <div className="main-homePage">
+        <br />
+        <InputIngredients />
+        <h1>QUI VANNO LE CARDS, IL FILTER E LE RICETTE </h1>
+        <FilterRecipes />
+        <Footer />
+      </div>
+    </div>
+  );
 }
